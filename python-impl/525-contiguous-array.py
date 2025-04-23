330c8cec0a73f2fe04fd95823321f18edb8f7f0c
Shah Nawaz <shuvo1824@hotmail.com>
2025-04-23 22:38:29 +0600

    chore[python]: 525. contiguous array

---
+++ python-impl/525-contiguous-array.py
@@ -0,0 +1,16 @@
+from typing import List
+
+
+def findMaxLength(nums: List[int]) -> int:
+    pass
+
+
+def main():
+    while True:
+        nums = list(
+            map(int, input("Enter numbers separated by spaces: ").split()))
+        findMaxLength(nums)
+
+
+if __name__ == "__main__":
+    main()
